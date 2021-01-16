993249bcbb4aa7c815290db1688bc96272167161
darkwolf <radek.pazderka1@gmail.com>
2021-01-16 21:21:48 +0100

    validate file

--- 02_caffe_classifier/validate.py
+++ 02_caffe_classifier/validate.py
@@ -1,1 +1,7 @@
 #TODO: validate
+import cv2
+
+
+if __name__ == '__main__':
+    img = cv2.imread(r"D:\MULTIMEDIA\videos\neural_videos\002\dataset\prepared\TRAIN\butterfly\ea36b70e2af2003ed1584d05fb1d4e9fe777ead218ac104497f5c97faee9bdba_640.jpg")
+    pass
