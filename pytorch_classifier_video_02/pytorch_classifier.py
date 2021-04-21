b61d16f1cc9a2e56587e2bcc78429387300e45ba
darkwolf <radek.pazderka1@gmail.com>
2021-04-21 21:55:28 +0200

    preparing second course

---
+++ pytorch_classifier_video_02/pytorch_classifier.py
@@ -0,0 +1,87 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+import torch.optim as optim
+import torch.nn as nn
+from pytorch_classifier_video_02.model import net
+
+
+t = torch.rand(4,4)
+t_numpy = t.numpy()
+b = t.view(-1,8)
+b_numpy = b.numpy()
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 4
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=0)
+
+
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# # functions to show an image
+#
+#
+# def imshow(img):
+#     img = img / 2 + 0.5     # unnormalize
+#     npimg = img.numpy()
+#     plt.imshow(np.transpose(npimg, (1, 2, 0)))
+#     plt.show()
+#
+#
+# # get some random training images
+# dataiter = iter(trainloader)
+# images, labels = dataiter.next()
+#
+# # show images
+# imshow(torchvision.utils.make_grid(images))
+# # print labels
+# print(' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
