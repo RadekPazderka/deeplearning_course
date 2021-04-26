871507a9daecfde1124fe822df317d927cfd7ef1
darkwolf <radek.pazderka1@gmail.com>
2021-04-26 12:40:32 +0200

    train update

--- pytorch_classifier_video_02/animal_trainer.py
+++ pytorch_classifier_video_02/animal_trainer.py
@@ -66,7 +66,8 @@ class AnimalTrainer(object):
                 loss.backward()
                 optimizer.step()
 
-                scheduler.step(epoch)
+                scheduler.step()
+
             checkpoint_path = self._save_checkpoint(epoch, vgg16)
             self._validate_checkpoint(checkpoint_path)
 
--- pytorch_classifier_video_02/main.py
+++ pytorch_classifier_video_02/main.py
@@ -6,9 +6,10 @@ from pytorch_classifier_video_02.animal_trainer import AnimalTrainer
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--train_dir', dest="train_dir", type=str, help='Directory of train images.')
-    parser.add_argument('--val_dir', dest="val_dir", type=str, help='Directory of val images.')
-    parser.add_argument('--checkpoint_dir', dest="checkpoint_dir", type=str, help='Directory of model checkpoints.')
+    parser.add_argument('--train_dir', dest="train_dir", type=str, required=True, help='Directory of train images.')
+    parser.add_argument('--val_dir', dest="val_dir", type=str, required=True, help='Directory of val images.')
+    parser.add_argument('--checkpoint_dir', dest="checkpoint_dir", type=str, required=True, help='Directory of model checkpoints.')
+    parser.add_argument('--pretrained_model', dest="pretrained_model", default=None, type=str, help='Directory of model checkpoints.')
 
     args = parser.parse_args()
     return args
@@ -20,7 +21,7 @@ if __name__ == '__main__':
     args = parse_args()
 
 
-    animal_trainer = AnimalTrainer(args.train_dir, args.val_dir, args.checkpoint_dir)
+    animal_trainer = AnimalTrainer(args.train_dir, args.val_dir, args.checkpoint_dir, args.pretrained_model)
 
     animal_trainer.train()
     animal_trainer.validate()
