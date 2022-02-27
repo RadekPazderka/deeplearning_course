952bd651998a764102f5cdb81ef738e520172eb3
radek <radek.pazderka@roboauto.cz>
2022-02-27 19:16:25 +0100

    tqdm prettier

--- pytorch_classifier_video_02/animal_trainer.py
+++ pytorch_classifier_video_02/animal_trainer.py
@@ -52,28 +52,33 @@ class AnimalTrainer(object):
         optimizer = torch.optim.Adam(vgg16.parameters(), lr=self.LEARNING_RATE)
 
         # Train the model
+
+
         for epoch in range(self.EPOCH):
 
             avg_loss = 0
             cnt = 0
-            for images, labels in tqdm(train_loader, desc="Epoch: {}".format(epoch)):
-                if self._mode == "GPU":
-                    images = images.cuda()
-                    labels = labels.cuda()
+            with tqdm(train_loader, colour='blue') as logger:
+                for images, labels in train_loader:
 
-                # Forward + Backward + Optimize
-                optimizer.zero_grad()
-                outputs = vgg16(images)
-                loss = cost(outputs, labels)
-                avg_loss += loss.data
-                cnt += 1
-                print("[E: {}] loss: {}, avg_loss: {}, best checkpoint: {} ({} %)".format(epoch,
-                                                                                          loss.data,
-                                                                                          avg_loss / cnt,
-                                                                                          os.path.basename(self._best_checkpoint),
-                                                                                          self._best_validitation_score))
-                loss.backward()
-                optimizer.step()
+                    if self._mode == "GPU":
+                        images = images.cuda()
+                        labels = labels.cuda()
+
+                    # Forward + Backward + Optimize
+                    optimizer.zero_grad()
+                    outputs = vgg16(images)
+                    loss = cost(outputs, labels)
+                    avg_loss += loss.data
+                    cnt += 1
+                    loss.backward()
+                    optimizer.step()
+                    logger.set_description("[E: {}] loss: {}, avg_loss: {}, best checkpoint: {} ({} %)".format(epoch,
+                                                                                              loss.data,
+                                                                                              avg_loss / cnt,
+                                                                                              os.path.basename(self._best_checkpoint),
+                                                                                              self._best_validitation_score))
+                    logger.update(1)
 
             checkpoint_path = self._save_checkpoint(epoch, vgg16)
             self._validate_checkpoint(checkpoint_path)
@@ -90,12 +95,13 @@ class AnimalTrainer(object):
         if (pretrained_checkpoint is None):
             vgg16 = VGG16(10)
         else:
-            print("[INFO] Loading pretrained weights. ({})".format(pretrained_checkpoint))
+            # print("[INFO] Loading pretrained weights. ({})".format(pretrained_checkpoint))
             vgg16 = torch.load(pretrained_checkpoint)
         return vgg16
 
 
     def _validate_checkpoint(self, checkpoint_path: str) -> None:
+        checkpoint_name = os.path.basename(checkpoint_path)
         vgg16 = self._get_vgg_model(checkpoint_path)
         vgg16.cuda()
         vgg16.eval()
@@ -104,21 +110,27 @@ class AnimalTrainer(object):
         total = 0
         testData = dsets.ImageFolder(self._dataset_val_dir, self._transform)
         testLoader = torch.utils.data.DataLoader(dataset=testData, batch_size=self.BATCH_SIZE, shuffle=False)
+        with tqdm(testLoader, colour='green') as logger:
+            for images, labels in testLoader:
+                if self._mode == "GPU":
+                    images = images.cuda()
+                outputs = vgg16(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted.cpu() == labels).sum()
+                curr_acc = (100.0 * float(correct) / float(total))
 
-        for images, labels in tqdm(testLoader):
-            if self._mode == "GPU":
-                images = images.cuda()
-            outputs = vgg16(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted.cpu() == labels).sum()
+                logger.set_description("[Checkpoint: {}] Current acc: {:.2f} (correct: {} / {})".format(checkpoint_name, curr_acc, correct, total))
+                logger.update(1)
 
-        avg_acc = (100.0 * float(correct) / float(total))
+            avg_acc = (100.0 * float(correct) / float(total))
+            # logger.set_description("[Checkpoint: {}]Current acc: {:.2f} (correct: {} / {})".format(checkpoint_name, curr_acc, correct, total))
+
         if avg_acc > self._best_validitation_score:
             self._best_validitation_score = avg_acc
             self._best_checkpoint = checkpoint_path
 
-        print("{} = avg acc: {}, correct: {}, total: {}".format(os.path.basename(checkpoint_path), avg_acc, correct, total))
+        # print("{} = avg acc: {}, correct: {}, total: {}".format(os.path.basename(checkpoint_path), avg_acc, correct, total))
 
     def validate(self) -> None:
 
--- pytorch_classifier_video_02/train.py
+++ pytorch_classifier_video_02/train.py
@@ -7,19 +7,20 @@ except:
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train + val VGG model')
-    parser.add_argument('--train_dir', dest="train_dir", type=str, required=True, help='Directory of train images.')
-    parser.add_argument('--val_dir', dest="val_dir", type=str, required=True, help='Directory of val images.')
-    parser.add_argument('--checkpoint_dir', dest="checkpoint_dir", type=str, required=True, help='Directory of model checkpoints.')
+    parser.add_argument('--train_dir', dest="train_dir", default="./dataset/data/TRAIN", type=str,  help='Directory of train images.')
+    parser.add_argument('--val_dir', dest="val_dir", type=str, default="./dataset/data/VAL",  help='Directory of val images.')
+    parser.add_argument('--checkpoint_dir', dest="checkpoint_dir", default="./checkpoints", type=str,  help='Directory of model checkpoints.')
     parser.add_argument('--pretrained_model', dest="pretrained_model", default=None, type=str, help='Path to pretrained checkpoint / model.')
 
     args = parser.parse_args()
     return args
 
 if __name__ == '__main__':
-    vgg16 = torch.load(r"/data_slow/course/src/deeplearning_course/pytorch_classifier_video_02/checkpoints/vgg16_0048.pkl", map_location=torch.device('cpu'))
-    torch.save(vgg16.state_dict(), "new_checkpoint.pkl")
-    # args = parse_args()
-    # animal_trainer = AnimalTrainer(args.train_dir, args.val_dir, args.checkpoint_dir, args.pretrained_model)
-    #
-    # animal_trainer.train()
-    # animal_trainer.validate()
+    a = torch.cuda.is_available()
+    # vgg16 = torch.load(r"/data_slow/course/src/deeplearning_course/pytorch_classifier_video_02/checkpoints/vgg16_0048.pkl", map_location=torch.device('cpu'))
+    # torch.save(vgg16.state_dict(), "new_checkpoint.pkl")
+    args = parse_args()
+    animal_trainer = AnimalTrainer(args.train_dir, args.val_dir, args.checkpoint_dir, args.pretrained_model)
+
+    animal_trainer.train()
+    animal_trainer.validate()
